0bf2da4b75a9865b2235cb781d9e224c653a3493
Marcin Kubacki <neck12@interia.pl>
2019-11-06 16:23:43 +0100

    Updates

---
+++ precache-manifest.dd3aafcdfd91dd5d3a195f50b6c5d77a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea81175502dd0ba2e4353e83346880ad",
+    "url": "/app-spa/index.html"
+  },
+  {
+    "revision": "83563f6c0b58bae3c609",
+    "url": "/app-spa/static/css/main.f9841974.chunk.css"
+  },
+  {
+    "revision": "0804519ab3d75a173c37",
+    "url": "/app-spa/static/js/2.47f762ec.chunk.js"
+  },
+  {
+    "revision": "83563f6c0b58bae3c609",
+    "url": "/app-spa/static/js/main.4b2512cf.chunk.js"
+  },
+  {
+    "revision": "59c390fa753ffe7011db",
+    "url": "/app-spa/static/js/runtime-main.7df244c1.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-spa/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-spa/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-spa/static/media/header3.d9d36b65.jpg"
+  }
+]);
